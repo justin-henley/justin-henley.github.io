a968f3d1c33790b896b71da1f351f73b72d54b54
justin-henley <justin.a.henley@gmail.com>
2022-02-18 02:36:42 +0800

    factored out links into separate file

--- src/Tile.js
+++ src/Tile.js
@@ -1,54 +1,70 @@
 import React from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCode,
-  faArrowUpRightFromSquare,
-} from "@fortawesome/free-solid-svg-icons";
+
+import { vars } from "./text";
+import TileLinks from "./TileLinks";
 
 // Receives an object containing the preview image, title, description, links to live and code
 const Tile = ({ proj }) => {
+  let key = 1;
   return (
     <StyledTile>
-      {/* <Preview src={proj.img} alt="Preview image goes here" /> */}
-
-      <Content>
+      <Head>
         <Title>{proj?.title}</Title>
-        <Description>{proj?.description}</Description>
-        <Links>
-          <Link
-            href={proj?.live}
-            bgColor="maroon"
-            color="white"
-            hoverColor=""
-            hoverBgColor=""
-          >
-            <StyledIcon icon={faArrowUpRightFromSquare} />
-            <LinkText>Live</LinkText>
-          </Link>
-          <Link
-            href={proj?.code}
-            bgColor="green"
-            color="white"
-            hoverColor=""
-            hoverBgColor=""
-          >
-            <StyledIcon icon={faCode} />
-            <LinkText>Code</LinkText>
-          </Link>
-        </Links>
-      </Content>
+        <Tech>
+          {proj.tech.map((item) => {
+            return <TechItem key={`${proj.title}${item}`}>{item}</TechItem>;
+          })}
+        </Tech>
+      </Head>
+      <Description>{proj?.description}</Description>
+      <TileLinks proj={proj} />
     </StyledTile>
   );
 };
+
+const Head = styled.header`
+  margin: 0;
+  text-align: center;
+`;
+
+const Title = styled.h3`
+  display: block;
+  font-size: 1.5em;
+  color: ${vars.textDark};
+`;
+
+const Tech = styled.ul`
+  list-style: none;
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  margin: 0;
+`;
+
+const TechItem = styled.li`
+  font-size: 0.8em;
+  margin: 0 10px 0 10px;
+  color: ${vars.textLight};
+`;
+
+const Description = styled.p`
+  color: ${vars.textMedium};
+`;
+
 const StyledTile = styled.article`
   width: 100%;
   height: 12em;
 
   display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+
   /* border-radius: 5px; */
   padding: 1em;
-  margin: 0.5em;
+  margin: 1em;
   background-color: white;
   box-shadow: 0 5px 10px gray;
 
@@ -56,67 +72,5 @@ const StyledTile = styled.article`
     width: 40%;
   }
 `;
-
-const Image = ({ className, src, alt }) => (
-  <img className={className} src={src} alt={alt} />
-);
-
-const Preview = styled(Image)`
-  display: block;
-  background-color: darkgray;
-  margin-right: 0.5em;
-  border-radius: 5px;
-  text-align: center;
-`;
-
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-`;
-
-const Title = styled.h3`
-  display: block;
-  font-size: 1.5em;
-`;
-
-const Description = styled.p``;
-
-const Links = styled.div``;
-
-const LinkText = styled.p`
-  display: inline-block;
-  vertical-align: middle;
-`;
-
-const Link = styled.a.attrs((props) => ({
-  target: "blank",
-  rel: "noopener noreferrer",
-  href: props?.href,
-}))`
-  display: inline-block;
-  text-decoration: none;
-  color: ${(props) => props?.color};
-  background-color: ${(props) => props?.bgColor};
-  border-radius: 5px 5px 5px 5px;
-  transition: all 0.2s ease-in-out;
-  padding: 0.25em;
-  margin: 0.1em;
-
-  &:hover {
-    color: ${(props) => props?.hoverColor};
-    background-color: ${(props) => props?.hoverBgColor};
-    transform: scale(1.2);
-  }
-`;
-
-const Icon = ({ className, children, icon }) => (
-  <FontAwesomeIcon className={className} icon={icon}>
-    {children}
-  </FontAwesomeIcon>
-);
-
-const StyledIcon = styled(Icon)``;
 
 export default Tile;
