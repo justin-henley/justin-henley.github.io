e9a9695ad77671317c8459d2cdfa7ad847f72644
justin-henley <justin.a.henley@gmail.com>
2022-02-18 02:36:16 +0800

    created footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,30 @@
 import React from "react";
+import styled from "styled-components";
+import { copyright, vars } from "./text";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  /*   const date = new Date(); */
+  return (
+    <Wrapper>
+      {/* &copy;{date.getFullYear()} */}
+      <Link href={copyright.source}>{copyright.name}</Link>
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.footer`
+  margin: 3em auto 1em auto;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+  color: ${vars.textLight};
+  font-weight: bold;
+
+  &:hover {
+    color: ${vars.textDark};
+    text-decoration: underline;
+  }
+`;
 
 export default Footer;
