4872c8a4d65f1c158b5bc52d4d91296e7fe8db41
Justin Henley <justin.a.henley@gmail.com>
2022-06-14 01:53:15 +0800

    Added link to favicon in footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,19 +1,30 @@
-import React from "react";
-import styled from "styled-components";
-import { copyright, vars } from "./text";
+import React from 'react';
+import styled from 'styled-components';
+import { copyright, vars } from './text';
 
 const Footer = () => {
   /*   const date = new Date(); */
   return (
     <Wrapper>
       {/* &copy;{date.getFullYear()} */}
-      <Link href={copyright.source}>{copyright.name}</Link>
+      <Link target="_blank" rel="noopener noreferrer" href={copyright.source}>
+        {copyright.name}
+      </Link>
+      <br />
+      <Link target="_blank" rel="noopener noreferrer" href="https://icons8.com/icon/kSC2OcpmW6rC/portfolio">
+        Portfolio icon
+      </Link>
+      &nbsp;
+      <Link target="_blank" rel="noopener noreferrer" href="https://icons8.com">
+        by Icons8
+      </Link>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.footer`
   margin: 3em auto 1em auto;
+  text-align: center;
 `;
 
 const Link = styled.a`
